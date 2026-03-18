e1575a99a2c56bd0b4b0b996abb93bee039aa57b
sonam choki <08250283.sce@rub,edu.bt>
2026-03-18 10:03:46 +0600

    string statement

---
+++ stringEx.py
@@ -0,0 +1,12 @@
+print("hello")
+print("hello")
+multiplelines= '''This is is a string 
+that spans multiple lines.'''
+print ("multiplelines")
+a = 1
+b = 2
+c = a + b
+print(type(c))
+c = str (c)
+print(type(c))
+
