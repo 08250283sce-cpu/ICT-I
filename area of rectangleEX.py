1def857a00c662cdc4d734eb4da938e993f8386b
sonam choki <08250283.sce@rub,edu.bt>
2026-03-20 15:28:25 +0600

    area of the rectangle

---
+++ area of rectangleEX.py
@@ -0,0 +1,9 @@
+l = float(input("enter the length of the rectangle:"));
+b = float(input("enter the breath of the rectangle:"));
+area = l*b
+print(" the area of the rectangle is :" + str(area))
+
+
+
+
+      
