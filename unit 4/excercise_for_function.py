0dbefc94b34c0a3632f4db80e3350a92ce851c83
sonam choki <08250283.sce@rub,edu.bt>
2026-04-23 13:31:04 +0600

    function excercise

---
+++ unit 4/excercise_for_function.py
@@ -0,0 +1,35 @@
+
+mark_1 = float(input("Enter mark1: "))
+mark_2 = float(input("Enter mark2: "))
+mark_3 = float(input("Enter mark3: "))
+
+def calculate_marks(mark_1, mark_2, mark_3):
+    return mark_1 + mark_2 + mark_3
+
+def calculate_average(mark_1, mark_2, mark_3):
+    return (mark_1 + mark_2 + mark_3) / 3
+
+total = calculate_marks(mark_1, mark_2, mark_3)
+average = calculate_average(mark_1, mark_2, mark_3)
+
+print("The total for the 3 subjects is:", total)
+print("The average for the 3 subjects is:", average)
+
+if average >= 50:
+    print("Pass")
+else:
+    print("Fail")
+
+
+
+number = int(input("Enter a number: "))
+def check_even_odd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+
+
+print("The number is:", check_even_odd(number))
+   
